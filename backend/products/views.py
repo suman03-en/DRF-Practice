bb1aa6924a0581a169ec6afe480b22b59a4125c1
suman phuyal <sfuyal59@gmail.com>
2026-01-13 20:26:58 +0545

    feat: add basic token authentication in api

--- backend/products/views.py
+++ backend/products/views.py
@@ -6,11 +6,12 @@ from django.shortcuts import get_object_or_404
 from .permissions import IsStaffEditorPermission
 from .models import Product
 from .serializers import ProductSerializer
+from api.authentication import TokenAuthentication
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    authentication_classes =[authentication.SessionAuthentication]
+    authentication_classes =[authentication.SessionAuthentication, TokenAuthentication]
     permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
 
@@ -35,8 +36,9 @@ product_detail_view = ProductDetailAPIView.as_view()
 product_list_create_view = ProductListCreateAPIView.as_view()
 product_update_view = ProductUpdateAPIView.as_view()
 product_delete_view = ProductDeleteAPIView.as_view()
-#function based api views
 
+
+#function based api views
 @api_view(["GET","POST"])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
